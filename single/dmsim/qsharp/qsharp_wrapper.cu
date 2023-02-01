aa555c8c39b8a7113a98f3b63ac0250287b443d2
Meng <meng.wang@pnnl.gov>
2023-02-01 12:30:43 -0800

    qsharp wrapper dump qasm added

--- single/dmsim/qsharp/qsharp_wrapper.cu
+++ single/dmsim/qsharp/qsharp_wrapper.cu
@@ -1,5 +1,5 @@
 // ---------------------------------------------------------------------------
-// NWQSim: Northwest Quantum Simulation Environment 
+// NWQSim: Northwest Quantum Simulation Environment
 // ---------------------------------------------------------------------------
 // Ang Li, Senior Computer Scientist
 // Pacific Northwest National Laboratory(PNNL), U.S.
@@ -26,30 +26,32 @@
 #include "../src/util.h"
 #include "../src/dmsim_nvgpu_sin.cuh"
 
+#include "dump_qasm.hpp"
+
 using namespace NWQSim;
 
-//class SVSimSimulator final : public Microsoft::Quantum::IRuntimeDriver, 
-//public Microsoft::Quantum::IQuantumGateSet
+// class SVSimSimulator final : public Microsoft::Quantum::IRuntimeDriver,
+// public Microsoft::Quantum::IQuantumGateSet
 
 class SVSimSimulator final : public Microsoft::Quantum::IRuntimeDriver
 {
 public:
     Result zero = reinterpret_cast<Result>(0xface0000);
     Result one = reinterpret_cast<Result>(0xface1000);
-    char* qbase = reinterpret_cast<char*>(0xfce20000);
+    char *qbase = reinterpret_cast<char *>(0xfce20000);
 
     //================= Basic ==================//
-    IdxType to_qubit(QubitIdType Q) 
+    IdxType to_qubit(QubitIdType Q)
     {
         CHECK_NULL_POINTER(Q);
-        IdxType q = static_cast<IdxType>(reinterpret_cast<char*>(Q) - qbase);
+        IdxType q = static_cast<IdxType>(reinterpret_cast<char *>(Q) - qbase);
         return q;
     }
-    QubitIdType from_qubit(IdxType qubit) 
+    QubitIdType from_qubit(IdxType qubit)
     {
         return reinterpret_cast<QubitIdType>(qbase + qubit);
     }
-    SVSimSimulator():sim(NULL)
+    SVSimSimulator() : sim(NULL)
     {
         sim = new Simulation();
         n_qubits = 0;
@@ -64,29 +66,31 @@ public:
     {
         throw std::logic_error("QubitToString not_implemented");
     }
-    
+
     //================= RuntimeDriver ==================//
-    QubitIdType AllocateQubit() override 
+    QubitIdType AllocateQubit() override
     {
         ++n_qubits;
         if (n_qubits > N_QUBIT_SLOT)
             throw std::logic_error("Request qubits more than slots!");
         sim->AllocateQubit();
-        return from_qubit(n_qubits-1);
+        return from_qubit(n_qubits - 1);
     }
     void ReleaseQubit(QubitIdType Q) override
     {
         sim->ReleaseQubit();
         --n_qubits;
-        if (n_qubits == 0) sim->reset_sim();
+        if (n_qubits == 0)
+            sim->reset_sim();
     }
-    void ReleaseResult(Result result) override {} 
+    void ReleaseResult(Result result) override {}
 
-    bool AreEqualResults(Result r1, Result r2) override 
+    bool AreEqualResults(Result r1, Result r2) override
     {
         return (r1 == r2);
     }
-    ResultValue GetResultValue(Result result) override {
+    ResultValue GetResultValue(Result result) override
+    {
         return (result == one) ? Result_One : Result_Zero;
     }
     Result UseZero() override { return zero; }
@@ -95,88 +99,110 @@ public:
     IdxType ControlledToMask(long numControls, QubitIdType controls[])
     {
         IdxType mask = 0;
-        for (long i=0; i<numControls; i++)
+        for (long i = 0; i < numControls; i++)
         {
-            mask = mask | ((IdxType)1<<to_qubit(controls[i]));
+            mask = mask | ((IdxType)1 << to_qubit(controls[i]));
         }
         return mask;
     }
 
     //================= Basis Gates ==================//
-    void X(QubitIdType Qtarget) 
+    void X(QubitIdType Qtarget)
     {
-        sim->X(to_qubit(Qtarget));
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "x", target);
+#endif
+        sim->X(target);
     }
-    void ID(QubitIdType Qtarget) 
+    void ID(QubitIdType Qtarget)
     {
-        sim->ID(to_qubit(Qtarget));
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "id", target);
+#endif
+        sim->ID(target);
     }
-    void RZ(double theta, QubitIdType Qtarget) 
+    void RZ(double theta, QubitIdType Qtarget)
     {
-        sim->RZ(theta, to_qubit(Qtarget));
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "rz", target, -1, 1, theta);
+#endif
+        sim->RZ(theta, target);
     }
-    void SX(QubitIdType Qtarget) 
+    void SX(QubitIdType Qtarget)
     {
-        sim->SX(to_qubit(Qtarget));
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "sx", target);
+#endif
+        sim->SX(target);
     }
-    void CX(QubitIdType Qcontrol, QubitIdType Qtarget) 
+    void CX(QubitIdType Qcontrol, QubitIdType Qtarget)
     {
-        sim->CX(to_qubit(Qcontrol), to_qubit(Qtarget));
+        IdxType ctrl = to_qubit(Qcontrol);
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "cx", target, ctrl);
+#endif
+        sim->CX(ctrl, target);
     }
     Result measure(QubitIdType Qtarget)
-    { 
-        IdxType res = sim->measure(to_qubit(Qtarget));
-        return (res==(IdxType)1) ? UseOne() : UseZero();
+    {
+        IdxType target = to_qubit(Qtarget);
+#ifdef DUMP_QASM
+        push_qasmstr(DUMP_QASM, "measure", target, 0, 0, 0, n_qubits);
+#endif
+        IdxType res = sim->measure(target);
+        return (res == (IdxType)1) ? UseOne() : UseZero();
     }
+
 private:
-    Simulation* sim;
+    Simulation *sim;
     IdxType n_qubits;
 };
 
-
-extern "C" Microsoft::Quantum::IRuntimeDriver* GetSVSim() 
+extern "C" Microsoft::Quantum::IRuntimeDriver *GetSVSim()
 {
-    static Microsoft::Quantum::IRuntimeDriver* g_iqa = nullptr;
-    if(!g_iqa) 
+    static Microsoft::Quantum::IRuntimeDriver *g_iqa = nullptr;
+    if (!g_iqa)
     {
         g_iqa = new SVSimSimulator{};
     }
     return g_iqa;
 }
 
-static SVSimSimulator* getSim()
+static SVSimSimulator *getSim()
 {
-    return dynamic_cast<SVSimSimulator*>(Microsoft::Quantum::GlobalContext()->GetDriver());
+    return dynamic_cast<SVSimSimulator *>(Microsoft::Quantum::GlobalContext()->GetDriver());
 }
 
 extern "C"
 {
     void __quantum__qis__x__body(QubitIdType qubit) // NOLINT
     {
-        //printf("Called X!\n");
+        // printf("Called X!\n");
         getSim()->X(qubit);
     }
     void __quantum__qis__rz__body(double theta, QubitIdType qubit)
     {
-        //printf("Called RZ!\n");
+        // printf("Called RZ!\n");
         getSim()->RZ(theta, qubit);
     }
     void __quantum__qis__sqrtx__body(QubitIdType qubit)
     {
-        //printf("Called SX!\n");
+        // printf("Called SX!\n");
         getSim()->SX(qubit);
     }
     void __quantum__qis__cnot__body(QubitIdType ctrl, QubitIdType qubit)
     {
-        //printf("Called CX!\n");
+        // printf("Called CX!\n");
         getSim()->CX(ctrl, qubit);
     }
     Result __quantum__qis__m__body(QubitIdType qubit)
     {
-        //printf("Called M!\n");
+        // printf("Called M!\n");
         return getSim()->measure(qubit);
     }
-
 }
-
-
