5f0af772a7f1d61c06f468b66902fc465d266ab0
Meng Wang <meng.wang@pnnl.gov>
2023-08-15 10:38:25 -0700

    measure added

--- data/spin_model/qiskit_execute.py
+++ data/spin_model/qiskit_execute.py
@@ -6,27 +6,22 @@ import time
 def execute_on_gpu(qasm_filename, shots):
     # Load the QASM file into a Quantum Circuit
     qc = QuantumCircuit.from_qasm_file(qasm_filename)
-    
-    simulator_gpu = Aer.get_backend('aer_simulator')
-    simulator_gpu.set_options(device='GPU')
-    
-    # Transpile the circuit for the simulator
-    compiled_circuit = transpile(qc, simulator_gpu)
-    
- #   qc = qc.decompose().decompose()
+    qc.measure_all() 
+    simulator_gpu = Aer.get_backend('aer_simulator_statevector_gpu',  cuStateVec_enable=True)
+    #simulator_gpu.set_options(device='GPU')
     
     # Record the start time
     start_time = time.time()
 
     # Execute the circuit on the simulator
-    result = simulator_gpu.run(compiled_circuit, shots=shots).result()
+    result = simulator_gpu.run(qc, shots=shots, fusion_enable=False, optimization_level=0).result()
 
     # Record the end time
     end_time = time.time()
 
     # Calculate the execution time
     execution_time = end_time - start_time
-   
+    print(result) 
 
 
     print(f"Execution Time: {execution_time:.4f} seconds, qiskit_reported: {result.to_dict()['time_taken']:.4f} seconds")
@@ -41,7 +36,7 @@ if __name__ == "__main__":
     shots = int(sys.argv[1])
     index = int(sys.argv[2])
     
-    qasm_filename = f"spin_model/spin_model_{index}.qasm"
+    qasm_filename = f"spin_model_{index}.qasm"
     result = execute_on_gpu(qasm_filename, shots)
     
 
