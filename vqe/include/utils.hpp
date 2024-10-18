72eb4b00fc4bcdef014bc32c04ca06b6368599be
Muqing Zheng <dzhengmq@gmail.com>
2024-10-18 13:08:39 -0700

    Make default value match with help in main.cpp

--- vqe/include/utils.hpp
+++ vqe/include/utils.hpp
@@ -95,7 +95,7 @@ namespace NWQSim{
     OptimizerSettings(): rel_tol(-1), 
                          abs_tol(-1),
                          stop_val(-MAXFLOAT),
-                         max_evals(50),
+                         max_evals(200), // MZ: changed from 50 to 200, to match the default in main.cpp. And 200 is still too small for H4 level with COBLY
                          max_time(-1),
                          lbound(-PI),
                          ubound(PI) {}
