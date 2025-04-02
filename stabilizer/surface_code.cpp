34dbe18a5cc85b50837c3f34e0b37dfb5ce5b630
Sean R Garner <garn195@deception02.pnl.gov>
2025-04-01 17:28:04 -0700

    data

--- stabilizer/surface_code.cpp
+++ stabilizer/surface_code.cpp
@@ -62,7 +62,7 @@ void measure_z_stabilizers(std::shared_ptr<NWQSim::Circuit> circuit, int distanc
 
 int main()
 {
-    for(int d = 117; d < 122; d+=2)
+    for(int d = 123; d < 152; d+=2)
     {
     int distance = d;
     int n_qubits = 2 * pow(distance, 2) + 1;
