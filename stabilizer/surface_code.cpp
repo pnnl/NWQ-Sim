65de9dbc102fe9b5515e3fa7400c2fa305a81183
seangarn32 <seangarn32@gmail.com>
2025-04-10 22:59:52 -0700

    surface code updates

--- stabilizer/surface_code.cpp
+++ stabilizer/surface_code.cpp
@@ -57,7 +57,7 @@ void measure_z_stabilizers(std::shared_ptr<NWQSim::Circuit> circuit, int distanc
 
 int main()
 {
-    for(int d = 127; d < 150; d+=2)
+    for(int d = 3; d < 150; d+=2)
     {
     int distance = d;
     int n_qubits = 2 * pow(distance, 2) + 1;
