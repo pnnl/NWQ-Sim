34c8b61c25f755df58168700091f073d419e6402
Muqing Zheng <dzhengmq@gmail.com>
2025-10-25 12:47:39 -0700

    Added missing dependencies on pybind11

--- vqe/perlmutter_vqe_setup.sh
+++ vqe/perlmutter_vqe_setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# git clone --recursive -b vqe_memory https://github.com/pnnl/NWQ-Sim.git $FOLDER_NAME
+FOLDER_NAME=${1:-"NWQ-Sim"}
+echo "Install NWQ-Sim in folder: $FOLDER_NAME"
 
-source ./environment/setup_perlmutter.sh
+git clone https://github.com/pnnl/NWQ-Sim.git $FOLDER_NAME
+cd ~/$FOLDER_NAME
+git submodule update --init --recursive vqe/nlopt
+git submodule update --init --recursive vqe/pybind11
+
+source ~/"$FOLDER_NAME"/environment/setup_perlmutter.sh
 module load python
 
 cd ~/"$FOLDER_NAME"
 
-rm -rf build
-mkdir build; cd build
+mkdir ~/$FOLDER_NAME/build
+cd ~/$FOLDER_NAME/build
 
-cmake .. -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DCMAKE_CUDA_HOST_COMPILER=CC -DCMAKE_BUILD_TYPE=Release && make -j10
+cmake .. -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DVQE_ENABLE_MPI=ON -DCMAKE_CUDA_HOST_COMPILER=CC -DCMAKE_BUILD_TYPE=Release && make -j16
 
 echo "NWQSim VQE Mem completed!"
